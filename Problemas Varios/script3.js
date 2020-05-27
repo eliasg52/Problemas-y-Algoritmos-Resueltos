e07696bbe8fb320602c9aa12f2990604d15f5248
eliasg52 <eliasgarcia81@gmail.com>
2020-05-27 18:45:26 -0300

    Ejercicio #3

---
+++ Problemas Varios/script3.js
@@ -0,0 +1,16 @@
+/*Desafío de programación #3: Imprimí la tabla de multiplicación del 7
+
+ 7x0 = 0
+ 7x1 = 7
+ ...
+ 7x9 = 63*/
+
+let multiplicadores = [0,1,2,3,4,5,6,7,8,9,10];
+let multiplicando = 7;
+let i;
+
+for (i = 0; i < multiplicadores.length; i++) {
+	producto = multiplicando * multiplicadores[i]
+}
+
+console.log('Tabla del 7: ' + producto);
